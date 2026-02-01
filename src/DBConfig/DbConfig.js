af7d4be42f753d5587e156dbe44dceff46d9ad9c
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2026-02-01 10:54:39 +0530

    Adding ENC DB URL

--- src/DBConfig/DbConfig.js
+++ src/DBConfig/DbConfig.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
 
 
 
 async function ConnectDB() {
-    mongoose.connect("mongodb://127.0.0.1:27017/NarutoTodo")
+    await mongoose.connect(process.env.DB_URL)
         .then(() => console.log("✅ MongoDB Connected"))
         .catch(err => console.error("❌ MongoDB Connection Error:", err));
 }
 
 module.exports = ConnectDB;
-
-
